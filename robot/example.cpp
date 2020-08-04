a4d6fae953ffc89dbe41852762466c09788d00f7
keywet06 <42454893+keywet06@users.noreply.github.com>
2020-08-04 18:09:24 +0800

    make AI example code files

---
+++ robot/example.cpp
@@ -0,0 +1,15 @@
+namespace AI {
+
+/*
+ * the main function to make step on 2048
+ * the array data is the numbers on 2048
+ * 1 is refered to 2; 2 is refered to 4; ...; n is refered to (1 << n); ...
+ * the return type is:
+ * 0: up
+ * 1: right
+ * 2: down
+ * 3: left
+ */
+int solve(int data[4][4]) { /* some code */ }
+
+}  // namespace AI
