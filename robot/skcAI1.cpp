23dc7fe565972ca4c28a9704db2e641b65a1c815
keywet06 <keywet06@tom.com>
2020-08-05 16:07:15 +0800

    fix format problem

--- robot/skcAI1.cpp
+++ robot/skcAI1.cpp
@@ -1,14 +1,16 @@
 #include "api.hpp"
-int cnt=0;
-int solve(int data[4][4]){
+int cnt = 0;
+int solve(int data[4][4]) {
     int temp;
-    if(cnt&1) temp=3;
-    else temp=2;
-    if(!test(temp)) temp=5-temp;
-    if(!test(temp)){
-        temp=1;
-        if(!test(temp)) temp=0;
-        cnt=-1;
+    if (cnt & 1){
+        temp = 3;}
+    else{
+        temp = 2;}
+    if (!test(temp)) temp = 5 - temp;
+    if (!test(temp)) {
+        temp = 1;
+        if (!test(temp)) temp = 0;
+        cnt = -1;
     }
     cnt++;
     return temp;
